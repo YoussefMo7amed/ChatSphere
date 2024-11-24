a440cd7949a5fbbeae01a049e096922bd27e4845
Youssef <Youssef.Mo7amed@gmail.com>
2024-11-24 14:48:12 +0200

    Install dev packages for testing and swagger package for API docs

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
 // src/index.js
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
+const api = require("./app/routes/index");
 require("dotenv").config();
-
-const port = process.env.PORT || 3000;
-const dbUrl = process.env.DATABASE_URL;
-const redisUrl = process.env.REDIS_URL;
-const elasticUrl = process.env.ELASTIC_URL;
-
-// const { databaseConfig } = require("../config/database");
-// const { REDIS_URL } = require("../config/redis");
-// const { elasticsearchConfig } = require("../config/elasticsearch");
-
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-let cnt = 0;
-app.get("/", (req, res) => {
-  cnt++;
-  console.log("weolcon dfkodnkofdjf");
-  res.send(
-    `<h1>Welcome to the chat system!<h1> <br> <h2>you visited us ${cnt} times<h2>`
-  );
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Endpoint to serve Swagger JSON
+app.get("/swagger.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
 });
 
-const redis = require("redis");
-const client = redis.createClient({ url: process.env.REDIS_URL });
+let cnt = 0;
+app.get("/", (req, res) => {
+    cnt++;
+    res.send(`
+        <h1>Welcome to the chat system!</h1>
+        <br>
+        <h2>You visited us ${cnt} times</h2>
+        <p>Checkout the API documentation on <a href="/api-docs">/api-docs</a></p>
+    `);
+});
 
-client.on("connect", () => console.log("Connected to Redis"));
-client.on("error", (err) => console.error("Redis Error:", err));
-
-client.connect();
+app.use(api);
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
