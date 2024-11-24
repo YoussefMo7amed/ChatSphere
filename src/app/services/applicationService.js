62a41fa4c911c49b8ec03c88198811a0cd4535bf
Youssef <Youssef.Mo7amed@gmail.com>
2024-11-24 14:46:12 +0200

    Service: handle errors and integrate with repo changes

--- src/app/services/applicationService.js
+++ src/app/services/applicationService.js
@@ -1,7 +1,20 @@
 const applicationRepository = require("../repositories/applicationRepository");
 const { generateToken } = require("../../utils/tokenGenerator");
-
+const { paginationBuilder, NotFoundError } = require("../../utils/shared");
 // todo , create pagination, update the swagger
+
+/**
+ * Formats an application object into a response object, only exposing the name and token.
+ * @param {Application} application - The application object to be formatted.
+ * @returns {Object} - The formatted response object.
+ */
+const responseFormatter = (application) => {
+    return {
+        name: application.name,
+        token: application.token,
+        chats_count: application.chats_count,
+    };
+};
 
 class ApplicationService {
     /**
@@ -10,20 +23,37 @@ class ApplicationService {
      * @returns {Promise<Application>} - The newly created application.
      */
     async createApplication(name) {
-        const token = generateToken();
-        return await applicationRepository.create({
-            name,
-            token,
-            chats_count: 0,
-        });
+        try {
+            const application = await applicationRepository.create({
+                name,
+            });
+            return responseFormatter(application);
+        } catch (error) {
+            console.error(`Failed to create application: ${error.message}`);
+            throw new Error(`Failed to create application: ${error.message}`);
+        }
     }
 
     /**
      * Retrieves all applications.
      * @returns {Promise<Application[]>} - A promise that resolves to the list of applications.
      */
-    async getAllApplications() {
-        return await applicationRepository.findAll();
+    async getAllApplications(filterParams) {
+        try {
+            const { count, rows } =
+                await applicationRepository.findAllWithCount(filterParams);
+
+            const pagination = paginationBuilder(filterParams, count);
+
+            return { data: rows?.map(responseFormatter), pagination };
+        } catch (error) {
+            console.error(
+                `Failed to retrieve all applications: ${error.message}`
+            );
+            throw new Error(
+                `Failed to retrieve all applications: ${error.message}`
+            );
+        }
     }
 
     /**
@@ -32,7 +62,13 @@ class ApplicationService {
      * @returns {Promise<Application|null>} - A promise that resolves to the application if found, or null otherwise.
      */
     async getApplicationByToken(token) {
-        return await applicationRepository.findByToken(token);
+        try {
+            const application = await applicationRepository.findByToken(token);
+            return responseFormatter(application);
+        } catch (error) {
+            console.error(`Failed to retrieve application: ${error.message}`);
+            throw error;
+        }
     }
 
     /**
@@ -42,7 +78,15 @@ class ApplicationService {
      * @returns {Promise<Application>} - A promise that resolves to the updated application.
      */
     async updateApplicationByToken(token, updates) {
-        return await applicationRepository.updateByToken(token, updates);
+        try {
+            const { name } = updates;
+            const updatedApplication =
+                await applicationRepository.updateByToken(token, { name });
+            return responseFormatter(updatedApplication);
+        } catch (error) {
+            console.error(`Failed to update application: ${error.message}`);
+            throw error;
+        }
     }
 
     /**
@@ -50,8 +94,14 @@ class ApplicationService {
      * @param {string} token - The unique token of the application to be deleted.
      * @returns {Promise<void>} - A promise that resolves when the application is deleted.
      */
-    async deleteApplication(token) {
-        return await applicationRepository.deleteByToken(token);
+    async deleteApplicationByToken(token) {
+        try {
+            await applicationRepository.deleteByToken(token);
+        } catch (error) {
+            console.error(error);
+            console.error(`Failed to delete application: ${error.message}`);
+            throw error;
+        }
     }
 }
 
