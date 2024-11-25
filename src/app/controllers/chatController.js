b04f5f3870637b26c3a0af717bb60dc530637b4c
Youssef <Youssef.Mo7amed@gmail.com>
2024-11-25 20:27:36 +0200

    Refactor: chat controller

--- src/app/controllers/chatController.js
+++ src/app/controllers/chatController.js
@@ -16,7 +16,24 @@ class ChatController {
             const chat = await chatService.createChat(token);
             successResponse(res, chat, 201);
         } catch (error) {
+            console.error(error);
             errorResponse(res, error.message, error.statusCode ?? 400);
+        }
+    }
+
+    /**
+     * Get a chat by its number
+     * @param {Request} req
+     * @param {Response} res
+     */
+    async getChat(req, res) {
+        try {
+            const { token, number } = req.params;
+            const chat = await chatService.getChat(token, number);
+            successResponse(res, chat, 200);
+        } catch (error) {
+            console.error(error);
+            errorResponse(res, error.message, error.statusCode ?? 404);
         }
     }
 
@@ -35,7 +52,24 @@ class ChatController {
             });
             successResponse(res, data, 200, meta);
         } catch (error) {
-            errorResponse(res, error.message, error.statusCode ?? 400);
+            console.error(error);
+            errorResponse(res, error.message, error.statusCode ?? 404);
+        }
+    }
+
+    /**
+     * Delete a chat by its number
+     * @param {Request} req
+     * @param {Response} res
+     */
+    async delete(req, res) {
+        try {
+            const { token, number } = req.params;
+            await chatService.deleteChat(token, number);
+            successResponse(res, "Chat deleted", 204);
+        } catch (error) {
+            console.error(error);
+            errorResponse(res, error.message, error.statusCode ?? 404);
         }
     }
 }
