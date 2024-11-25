256bc0d212b934ea44caef11ada990d4bdd10e8e
Youssef <Youssef.Mo7amed@gmail.com>
2024-11-25 13:29:31 +0200

    service: adding cache layer to chat service

--- src/app/services/chatService.js
+++ src/app/services/chatService.js
@@ -1,56 +1,137 @@
 const chatRepository = require("../repositories/chatRepository");
-const applicationRepository = require("../repositories/applicationRepository");
-// todo , create pagination, update the swagger
+const applicationService = require("./applicationService");
+const { paginationBuilder, NotFoundError } = require("../../utils/shared");
+const { RedisClient, TIME_CONSTANTS } = require("../../utils/cacheUtils");
+const { sequelize } = require("../models");
+/**
+ * Formats a chat object into a response object, only exposing the number and messages count.
+ * The application token is also included if the application is provided.
+ * @param {Chat} chat - The chat object to be formatted.
+ * @param {Application} [application] - The application object, used to include the token in the response.
+ * @returns {Object} - The formatted response object.
+ */
+const responseFormatter = (chat, application) => {
+    return {
+        token: application?.token,
+        number: chat.number,
+        messages_count: chat.messages_count,
+    };
+};
+
+/**
+ * Creates the key for Redis given the application token and chat number.
+ * @param {string} applicationToken - The application token.
+ * @param {number} chatNumber - The chat number.
+ * @returns {string} - The Redis key.
+ */
+const createRedisMessageKey = (applicationToken, chatNumber) => {
+    return `chat:${applicationToken}:${chatNumber}`;
+};
 
 class ChatService {
-    /**
-     * Create a new chat for an application
-     * @param {string} applicationToken - The token of the application
-     * @returns {Promise<object>} - The created chat
-     */
     async createChat(applicationToken) {
-        // Find the application by its token
-        const application = await applicationRepository.findByToken(
-            applicationToken
-        );
-        if (!application) {
-            throw new Error("Application not found");
+        const transaction = await sequelize.transaction();
+
+        try {
+            const application = await applicationService.getApplicationByToken(
+                applicationToken,
+                false
+            );
+            if (!application) {
+                throw new Error("Application not found");
+            }
+
+            const chat = await chatRepository.create(
+                {
+                    application_id: application.id,
+                    messages_count: 0,
+                },
+                transaction
+            );
+
+            // Commit the transaction
+            await transaction.commit();
+            const response = responseFormatter(application, chat);
+            const cacheKey = createRedisMessageKey(
+                application.token,
+                chat.number
+            );
+            await RedisClient.setCache(
+                cacheKey,
+                JSON.stringify(response),
+                5 * TIME_CONSTANTS.MINUTE
+            );
+
+            return response;
+        } catch (error) {
+            await transaction.rollback();
+            console.error(`Error creating chat: ${error}`);
+            throw error;
         }
-
-        // handle
-        const nextChatNumber = application.chats_count + 1;
-
-        // Create the chat
-        const chat = await chatRepository.create({
-            number: nextChatNumber,
-            application_id: application.id,
-            messages_count: 0,
-        });
-
-        // Increment the chat count for the application
-        await applicationRepository.updateByToken(applicationToken, {
-            chats_count: nextChatNumber,
-        });
-
-        return chat;
     }
 
     /**
-     * Get all chats for an application
-     * @param {string} applicationToken - The token of the application
-     * @returns {Promise<Array>} - The list of chats
+     * Retrieve all chats for a specified application with pagination and filtering options.
+     * @param {string} applicationToken - The unique token of the application.
+     * @param {object} filterParams - The filtering and pagination parameters.
+     * @returns {Promise<object>} - An object containing the list of chats, pagination details, and application token.
+     * @throws {NotFoundError} - If the application is not found.
+     * @throws {Error} - If there is an error retrieving chats.
      */
-    async getChats(applicationToken) {
-        // Find the application by its token
-        const application = await applicationRepository.findByToken(
-            applicationToken
-        );
-        if (!application) {
-            throw new Error("Application not found");
-        }
+    async getChats(applicationToken, filterParams) {
+        try {
+            const cacheKey = `${createRedisMessageKey(
+                applicationToken,
+                "all"
+            )}:page:${filterParams.page}:limit:${filterParams.limit}`;
 
-        // Fetch all chats for the application
-        return await chatRepository.findAllByApplication(application.id);
+            let cachedResult;
+
+            try {
+                cachedResult = await RedisClient.getCache(cacheKey);
+            } catch (cacheError) {
+                console.warn(
+                    "Redis unavailable, could not fetch or cache result",
+                    cacheError
+                );
+            }
+            if (cachedResult) return JSON.parse(cachedResult);
+            const application = await applicationService.getApplicationByToken(
+                applicationToken,
+                false
+            );
+            if (!application) {
+                throw new NotFoundError("Application not found");
+            }
+            const { count, rows } = await chatRepository.findAllByApplicationId(
+                application.id,
+                filterParams
+            );
+            const pagination = paginationBuilder(filterParams, count);
+            const response = {
+                data: rows?.map(responseFormatter),
+                pagination,
+                token: application.token,
+            };
+
+            try {
+                await RedisClient.setCache(
+                    cacheKey,
+                    JSON.stringify(response),
+                    2 * TIME_CONSTANTS.MINUTE
+                );
+            } catch (cacheError) {
+                console.warn(
+                    "Redis unavailable, could not cache result",
+                    cacheError
+                );
+            }
+
+            return response;
+        } catch (error) {
+            console.error(error);
+            throw new Error(`Error getting chats: ${error.message}`);
+        }
     }
 
     /**
@@ -60,19 +141,55 @@ class ChatService {
      * @returns {Promise<object>} - The chat
      */
     async getChat(applicationToken, chatNumber) {
-        // Find the application by its token
-        const application = await applicationRepository.findByToken(
-            applicationToken
-        );
-        if (!application) {
-            throw new Error("Application not found");
-        }
+        try {
+            const application = await applicationService.getApplicationByToken(
+                applicationToken,
+                false
+            );
+            if (!application) {
+                throw new NotFoundError("Application not found");
+            }
 
-        // Fetch the chat by its number and application ID
-        return await chatRepository.findByNumberAndApplicationId(
-            chatNumber,
-            application.id
-        );
+            const cacheKey = createRedisMessageKey(
+                applicationToken,
+                chatNumber
+            );
+            let cachedChat;
+            try {
+                cachedChat = await RedisClient.getCache(cacheKey);
+            } catch (cacheError) {
+                console.warn(
+                    "Redis unavailable, could not fetch or cache chat",
+                    cacheError
+                );
+            }
+            if (cachedChat) return JSON.parse(cachedChat);
+
+            const chat = await chatRepository.findByNumberAndApplicationId(
+                application.id,
+                chatNumber
+            );
+
+            const response = responseFormatter(chat, application);
+
+            try {
+                await RedisClient.setCache(
+                    cacheKey,
+                    JSON.stringify(response),
+                    2 * TIME_CONSTANTS.MINUTE
+                );
+            } catch (cacheError) {
+                console.warn(
+                    "Redis unavailable, could not cache chat",
+                    cacheError
+                );
+            }
+
+            return response;
+        } catch (error) {
+            console.error(`Error getting chat: ${error}`);
+            throw error;
+        }
     }
 }
 
