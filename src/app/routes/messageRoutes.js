f19661df907c1bf53cf0bf81c6695f84fc6d2bf5
Youssef <Youssef.Mo7amed@gmail.com>
2024-11-27 23:18:41 +0200

    routes: adding search

--- src/app/routes/messageRoutes.js
+++ src/app/routes/messageRoutes.js
@@ -105,9 +105,64 @@ const messageController = require("../controllers/messageController");
  *                     type: string
  *                   body:
  *                     type: string
+ * /applications/{token}/chats/{chatNumber}/messages/search:
+ *   get:
+ *     tags: [messages]
+ *     summary: Search messages for a chat
+ *     description: Search messages for a chat
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         description: The token of the application
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: chatNumber
+ *         required: true
+ *         description: The number of the chat
+ *         schema:
+ *           type: integer
+ *       - in: query
+ *         name: query
+ *         schema:
+ *           type: string
+ *         description: The search query
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         default: 1
+ *         description: The page number to be retrieved
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         default: 10
+ *         description: The number of items per page
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 messages:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       number:
+ *                         type: integer
+ *                       token:
+ *                         type: string
+ *                       body:
+ *                         type: string
  */
 
 router.post("/", messageController.create);
 router.get("/", messageController.getMessages);
+router.get("/search", messageController.searchMessages);
 
 module.exports = router;
