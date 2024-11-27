ce35cb817363386ce5159250ba5ad32fac64221c
Youssef <Youssef.Mo7amed@gmail.com>
2024-11-27 23:23:42 +0200

    jobs: cancel worker thread

--- src/jobs/chatCountJob.js
+++ src/jobs/chatCountJob.js
@@ -1,29 +1,13 @@
 const { Worker } = require("worker_threads");
 const cron = require("node-cron");
+const { consumeChatCreation } = require("../workers/chatConsumer");
+const { getChannel } = require("../config/rabbitmq");
 
 function startChatCountJob() {
     console.log("Scheduling chat count job...");
-    //"0 * * * *",
+
     cron.schedule("*/10 * * * * *", () => {
-        console.log("Executing chat count job...");
-
-        const worker = new Worker("./jobs/chatCountWorker.js");
-
-        worker.on("message", (message) => {
-            console.log("Worker Message:", message);
-        });
-
-        worker.on("error", (error) => {
-            console.error("Worker Error:", error);
-        });
-
-        worker.on("exit", (code) => {
-            if (code !== 0) {
-                console.error(`Worker exited with error code: ${code}`);
-            } else {
-                console.log("Worker finished successfully.");
-            }
-        });
+        consumeChatCreation(getChannel);
     });
 
     console.log("Chat count job scheduled successfully.");
--- src/jobs/chatCountWorker.js
+++ src/jobs/chatCountWorker.js
@@ -1,18 +1,26 @@
 const { parentPort } = require("worker_threads");
 const amqp = require("amqplib");
 const { consumeChatCreation } = require("../workers/chatConsumer");
+const { getChannel } = require("../config/rabbitmq");
 
 async function runJob() {
     try {
-        // Create a new RabbitMQ connection for the worker thread
-        const connection = await amqp.connect(
-            process.env.RABBITMQ_URL || "amqp://localhost"
-        );
-        const channel = await connection.createChannel();
+        let channel;
+        try {
+            channel = await getChannel();
+        } catch (error) {
+            console.error("Failed to get RabbitMQ channel:", error);
+        }
+        // Use the provided channel or create a new one
+        const connection = !channel
+            ? await amqp.connect(process.env.RABBITMQ_URL || "amqp://localhost")
+            : null;
+        channel = channel ?? (await connection.createChannel());
 
-        // Pass the newly created channel to consumeChatCreation
+        // Pass the channel to the consumer
         await consumeChatCreation(channel);
-
+        // await channel.close();
+        // await connection.close();
         parentPort.postMessage("Chat count job completed successfully.");
     } catch (error) {
         console.error("Error in chat count job:", error);
@@ -21,5 +29,4 @@ async function runJob() {
         );
     }
 }
-
 runJob();
